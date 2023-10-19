6e4230fb95b7d4b0acc81d98020e83565f2a5951
andre.me.best@gmail.com <afas0>
2023-10-19 14:45:04 +0100

    Add project files.

---
+++ POOprojeto/Ticket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace POOprojeto
+{
+    internal class Ticket
+    {
+        // Properties with attributes
+        public int TicketId { get; set; }
+
+        [Required]
+        public string NomeCliente { get; set; }
+
+        [StringLength(100)]
+        public string TicketDescricao { get; set; }
+
+        public DateTime DataCriacao { get; set; }
+
+        public string EquipaIT { get; set; }
+
+
+    // Constructor
+    public Ticket(int ticketId, string nomeCliente, string ticketDescricao, DateTime dataCriacao, string equipaIT)
+        {
+            TicketId = ticketId;
+            NomeCliente = nomeCliente;
+            TicketDescricao = ticketDescricao;
+            DataCriacao = dataCriacao;
+            EquipaIT = equipaIT;
+        }
+    }
+}
